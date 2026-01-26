dd65eff772c3fb074aeed0621e4f329ac38bd926
Danish Javed <danishjaved636@gmail.com>
2026-01-26 18:08:46 +0530

    Adds more sections

--- pages/05_Forms.py
+++ pages/05_Forms.py
@@ -1,11 +1,141 @@
+import time
+
 import streamlit as st
 
 from utils import add_navigation, get_custom_css
 
 st.set_page_config(page_title="Forms in Streamlit | Danish Javed", layout="wide", page_icon="👨🏻‍💻")
 
+st.sidebar.title("Forms")
+st.sidebar.markdown("""
+- [Sample Code](#sample-code)
+- [Form Validation](#form-validation)
+""")
+
 st.title("Forms in Streamlit")
-st.caption("⚠️ Work in progress")
+
+st.write("""Used when
+ - Inputs need to be grouped together
+ - Multiple feilds are related and are to be validated togeather
+ - Submission is single event
+ 
+ Ex: Apply filters/chanes or signup screens or any other form
+ 
+ 
+ We have a few simple parameters.
+ -  clear_on_submit : default false
+ -  enter_to_submit : default true
+ 
+ > **Forms should have a key and duplicate keys aren't allowed.**
+ """)
+
+st.divider()
+
+st.header("Sample code")
+st.caption("I have used non default options for demo")
+
+st.code("""
+with st.form(key="login_form", clear_on_submit=True, enter_to_submit=False):
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submitted = st.form_submit_button("Login")
+
+if not submitted:
+    st.warning("Form not submitted yet")
+
+if submitted:
+    st.success("Form submitted")
+    st.write("email: ", email, "password: ", password)
+
+""")
+
+with st.form(key="login_form", clear_on_submit=True, enter_to_submit=False):
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submitted = st.form_submit_button("Login")
+
+if not submitted:
+    st.warning("Form not submitted yet")
+
+if submitted:
+    st.success("Form submitted")
+    st.write("email: ", email, "password: ", password)
+
+st.divider()
+
+st.header("Form validation")
+st.write("""
+We can also perform form validation when the form is submitted.
+""")
+
+st.code("""
+
+if "submitted" not in st.session_state:
+    st.session_state.submitted = False
+
+with st.form("login_form_validate"):
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submitted = st.form_submit_button("Login")
+
+if not submitted:
+    st.warning("Form not submitted yet")
+
+if submitted:
+    with st.empty():
+        st.info("Validating form inputs")
+        time.sleep(1)
+        if not email or not password:
+            st.error("Email and password are required")
+        elif "@" and "." not in email:
+            st.error("Email is not valid")
+        elif len(password) < 4:
+            st.error("Password is too short")
+        else :
+            st.success("Form submitted successfully")
+            time.sleep(3)
+            st.write(f\"""
+            - email : {email}
+            - password : {password}
+            \""")
+
+""")
+
+st.write("> **Renders this form below 👇🏻**")
+
+if "submitted" not in st.session_state:
+    st.session_state.submitted = False
+
+with st.form("login_form_validate"):
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+    submitted = st.form_submit_button("Login")
+
+if not submitted:
+    st.warning("Form not submitted yet")
+
+if submitted:
+    with st.empty():
+        st.info("Validating form inputs")
+        time.sleep(1)
+        if not email or not password:
+            st.error("Email and password are required")
+        elif "@" and "." not in email:
+            st.error("Email is not valid")
+        elif len(password) < 4:
+            st.error("Password is too short")
+        else :
+            st.success("Form submitted successfully")
+            time.sleep(3)
+            st.write(f"""
+            - email : {email}
+            - password : {password}
+            """)
+
+
 
 get_custom_css()
-add_navigation(previous_page="04_Input Widgets.py", previous_page_title="Input Widgets")
+add_navigation(previous_page="04_Input Widgets.py",
+               previous_page_title="Input Widgets",
+               next_page="pages/06_Data Display.py",
+               next_page_title="Data Display")
---
+++ pages/06_Data Display.py
@@ -0,0 +1,180 @@
+import streamlit as st
+import pandas as pd
+from matplotlib.dates import DateFormatter
+from typing_extensions import Format
+
+from utils import add_navigation, get_custom_css
+
+st.set_page_config(page_title="Data Display in Streamlit | Danish Javed", layout="wide", page_icon="👨🏻‍💻")
+
+
+st.title("Data Display")
+st.caption("Now we are hitting the real deal.")
+
+st.divider()
+
+st.header("Metrics")
+st.write("""
+Metrics displays the KPIs. We have some very useful options:
+- `label` : what the data is about
+- `value` : current value
+- `delta` : the change from last value
+- `delta_color` : "inverse" | "normal" | "off" -> By default, postive value is considered good and is green, we can change the nature with inverse or disable it with off
+- `delta-arrow` : "up" | "down" |  "off" | "auto"
+
+> **Checkout the sample code below 👇🏻**
+""")
+
+st.code("""
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric(
+        label="Revenue",
+        value="₹12.4M",
+        delta="+8%",
+        chart_data=[4, 10, 6, 8]
+    )
+    pass
+with col2:
+    st.metric(
+        label="Customer Exit",
+        value="12",
+        delta="2%",
+        chart_data=[20,15,10,12],
+        chart_type="bar",
+        delta_color="inverse"
+    )
+    pass
+with col3:
+    st.metric(
+        label="Weekly Sales",
+        value="200k",
+        delta="+12%",
+        chart_data=[50, 70, 90, 30, 78, 200],
+        chart_type="area"
+    )
+    pass
+""")
+
+st.write("> **Renders as below 👇🏻**")
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.metric(
+        label="Revenue",
+        value="₹12.4M",
+        delta="+8%",
+        chart_data=[4, 10, 6, 8]
+    )
+    pass
+with col2:
+    st.metric(
+        label="Customer Exit",
+        value="12",
+        delta="2%",
+        chart_data=[20,15,10,12],
+        chart_type="bar",
+        delta_color="inverse"
+    )
+    pass
+with col3:
+    st.metric(
+        label="Weekly Sales",
+        value="200k",
+        delta="+12%",
+        chart_data=[50, 70, 90, 30, 78, 200],
+        chart_type="area"
+    )
+    pass
+
+st.divider()
+
+st.header("Dataframe")
+st.write("""
+Its an interactive table that is:
+- sortable
+- scrollable
+- resizeable
+- searchable
+- downloadable
+
+&nbsp;  
+&nbsp;
+We have some cool optoins here
+- `height` : its better to provide a height to the table or else the large dataset will expand a lot
+- `use_container_width` : True by default, it set to False, the table will take width as per content width
+- `row_height` : Explicitly set height of each row
+- `hide_index` : By default row index are displayed, we can hide them with this option
+
+&nbsp;  
+
+**Sample code 👇🏻**
+
+""")
+
+st.code("""
+df = pd.DataFrame({
+    "date": pd.date_range("2026-01-01", periods=7).date,
+    "revenue": [120, 150, 100, 180, 160, 220, 200],
+    "users": [80, 95, 70, 110, 105, 140, 130],
+    "conversion_rate": [3.2, 3.5, 2.9, 4.1, 3.8, 4.5, 4.3]
+})
+st.dataframe(
+    df,
+    height=150,
+    use_container_width=True,
+    hide_index=True,
+    row_height=40,
+)
+""")
+
+st.write("> **Renders the table below 👇🏻**")
+
+df = pd.DataFrame({
+    "date": pd.date_range("2026-01-01", periods=7).date,
+    "revenue": [120, 150, 100, 180, 160, 220, 200],
+    "users": [80, 95, 70, 110, 105, 140, 130],
+    "conversion_rate": [3.2, 3.5, 2.9, 4.1, 3.8, 4.5, 4.3]
+})
+st.dataframe(
+    df,
+    height=150,
+    use_container_width=True,
+    hide_index=True,
+    row_height=40,
+)
+
+st.divider()
+
+st.header("Table")
+st.write("""Its a simple table, no additional features. We have 2 simple parameters
+- data
+- border : True|False|Horizontal
+&nbsp;  
+&nbsp;  
+**Sample code 👇🏻**
+""")
+
+st.code("""
+
+""")
+
+df = pd.DataFrame({
+    "date": pd.date_range("2026-01-01", periods=7).date,
+    "revenue": [120, 150, 100, 180, 160, 220, 200],
+    "users": [80, 95, 70, 110, 105, 140, 130],
+    "conversion_rate": [3.2, 3.5, 2.9, 4.1, 3.8, 4.5, 4.3]
+})
+st.table(data=df,border="horizontal")
+
+
+
+st.sidebar.title("Data Display")
+st.sidebar.markdown("""
+- [Metrics](#metrics)
+- [Dataframe](#dataframe)
+""")
+
+get_custom_css()
+add_navigation(previous_page="05_Forms.py", previous_page_title="Forms")
